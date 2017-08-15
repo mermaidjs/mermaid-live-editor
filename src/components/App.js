ab354e8ceed874a33cad50170c85a4222b172777
Tyler Long <tyler4long@gmail.com>
2017-08-15 21:14:52 +0800

    Use HashRouter instead

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
 
 import Home from './Home'
 import About from './About'
