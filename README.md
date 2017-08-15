a63e699466434da3c4ff2468638c759e4376a157
Tyler Long <tyler4long@gmail.com>
2017-08-15 17:54:57 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# react-router-demo
+
+## Setup
+
+```
+yarn install
+```
+
+
+## Development
+
+```
+yarn dev
+open http://localhost:8080
+```
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+
+const rules = [
+  {
+    test: /\.css$/,
+    use: ExtractTextPlugin.extract({
+      fallback: 'style-loader',
+      use: 'css-loader'
+    })
+  },
+  {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          ['env', { targets: { browsers: ['last 2 Chrome versions'] } }],
+          'react'
+        ]
+      }
+    }
+  }
+]
+
+const config = {
+  target: 'web',
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    path: __dirname,
+    filename: '[name].bundle.js'
+  },
+  module: { rules },
+  plugins: [
+    new ExtractTextPlugin('[name].bundle.css')
+  ],
+  devServer: {
+    contentBase: __dirname
+  }
+}
+
+export default [config]
