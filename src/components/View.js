245650eeb4191eb91af0dacc250c0f8ca7afd88d
Tyler Long <tyler4long@gmail.com>
2017-08-15 22:33:22 +0800

    Refactor code

--- src/components/View.js
+++ src/components/View.js
@@ -2,17 +2,13 @@ import React from 'react'
 
 class View extends React.Component {
   render () {
-    const { match } = this.props
-    return (
-      <div>
-        <h2>View</h2>
-        <div ref={div => { this.mermaidContainer = div }}>{window.atob(match.params.base64)}</div>
-      </div>
-    )
+    const { match: { params: { base64 } } } = this.props
+    const code = window.atob(base64)
+    return <div ref={div => { this.container = div }}>{code}</div>
   }
 
   componentDidMount () {
-    window.mermaid.init(undefined, this.mermaidContainer)
+    window.mermaid.init(undefined, this.container)
   }
 }
 
