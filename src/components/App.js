0027168eb556b7c0333c8a2c9f85fe30c9cab1dc
Tyler Long <tyler4long@gmail.com>
2017-08-15 19:31:50 +0800

    Refactor code

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+import Home from './Home'
+import About from './About'
+import Topics from './Topics'
+
+const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/topics'>Topics</Link></li>
+      </ul>
+
+      <div className='separator' />
+
+      <Route exact path='/' component={Home} />
+      <Route path='/about' component={About} />
+      <Route path='/topics' component={Topics} />
+    </div>
+  </Router>
+)
+
+export default App
--- src/components/Topics.js
+++ src/components/Topics.js
@@ -1,21 +1,5 @@
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
-
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-)
-
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-)
+import { Route, Link } from 'react-router-dom'
 
 const Topic = ({ match }) => (
   <div>
@@ -51,22 +35,4 @@ const Topics = ({ match }) => (
   </div>
 )
 
-const App = () => (
-  <Router>
-    <div>
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/about'>About</Link></li>
-        <li><Link to='/topics'>Topics</Link></li>
-      </ul>
-
-      <hr />
-
-      <Route exact path='/' component={Home} />
-      <Route path='/about' component={About} />
-      <Route path='/topics' component={Topics} />
-    </div>
-  </Router>
-)
-
-export default App
+export default Topics
