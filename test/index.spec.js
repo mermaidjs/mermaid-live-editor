a158e1e2e68cafd47fe35bb6379ee3e9358894e0
Tyler Long <tyler4long@gmail.com>
2017-09-02 15:37:43 +0800

    Refactor testing code

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,32 +4,33 @@ import React from 'react'
 import { mount } from 'enzyme'
 import { Input, Button } from 'antd'
 import { Link } from 'react-router-dom'
+import { Base64 } from 'js-base64'
 
 import 'mermaid'
 import App, { defaultCode } from '../src/components/App'
 
 let wrapper = null
-
-const verifyButtons = () => {
-  const buttons = wrapper.find(Button)
-  expect(buttons.length).toEqual(2)
-  const link = buttons.at(0).find(Link).first()
-  expect(link.props().children).toEqual('Link to View')
-  const a = buttons.at(1).find('a').first()
-  expect(a.props().children).toEqual('Download SVG')
-}
-
-const verifyTextArea = (text) => {
-  const textArea = wrapper.find(Input.TextArea).first()
-  expect(textArea).toBeDefined()
-  expect(textArea.props().value).toEqual(text)
-}
-
 beforeEach(() => {
   wrapper = mount(<App />)
 })
 
+const verifyTextArea = (code) => {
+  const textArea = wrapper.find(Input.TextArea).first()
+  expect(textArea).toBeDefined()
+  expect(textArea.props().value).toEqual(code)
+}
+
+const verifyButtons = (code) => {
+  const buttons = wrapper.find(Button)
+  expect(buttons.length).toEqual(2)
+  const link = buttons.at(0).find(Link).first()
+  expect(link.props().children).toEqual('Link to View')
+  expect(link.props().to).toEqual('/view/' + Base64.encode(code))
+  const a = buttons.at(1).find('a').first()
+  expect(a.props().children).toEqual('Download SVG')
+}
+
 test('/', () => {
   verifyTextArea(defaultCode)
-  verifyButtons()
+  verifyButtons(defaultCode)
 })
