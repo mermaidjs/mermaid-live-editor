9990d49150c35960831e16be255cc3985ea4b0e7
Tyler Long <tyler4long@gmail.com>
2017-09-12 22:36:35 +0800

    Upgrade to mermaid 7.0.15

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'mermaid'
-import 'mermaid/dist/themes/mermaid.forest.css'
 
 import App from './components/App'
 import './index.css'
