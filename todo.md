ad1f67998bbdb5a4b462d54300eb05d2e13af1dc
Tyler Long <tyler4long@gmail.com>
2017-12-24 16:01:15 +0800

    Update config files

--- todo.md
+++ todo.md
@@ -1,8 +1,8 @@
 - Setup CI and coveralls
 - Improve test coverage
-- Generate source map for easy debugging
 - `mermaid.init` should throw exception, no need to call `mermaid.parse`
 - mermaid inline style overrides html, body padding
     - trigger a syntax error to see how the padding changes
     - should rewrite mermaid inline style generation code
 - There is a global lodash `_`
+- Replace webpack with Parcel
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,31 +1,5 @@
 import ExtractTextPlugin from 'extract-text-webpack-plugin'
 
-const rules = [
-  {
-    test: /\.css$/,
-    use: ExtractTextPlugin.extract({
-      fallback: 'style-loader',
-      use: 'css-loader'
-    })
-  },
-  {
-    test: /\.js$/,
-    exclude: /node_modules/,
-    use: {
-      loader: 'babel-loader',
-      options: {
-        presets: [
-          'env',
-          'react'
-        ],
-        plugins: [
-          ['import', { libraryName: 'antd', style: 'css' }]
-        ]
-      }
-    }
-  }
-]
-
 const config = {
   target: 'web',
   entry: {
@@ -35,13 +9,40 @@ const config = {
     path: __dirname,
     filename: '[name].bundle.js'
   },
-  module: { rules },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              'env',
+              'react'
+            ],
+            plugins: [
+              ['import', { libraryName: 'antd', style: 'css' }]
+            ]
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     new ExtractTextPlugin('[name].bundle.css')
   ],
   devServer: {
     contentBase: __dirname
-  }
+  },
+  devtool: 'source-map'
 }
 
-export default [config]
+export default config
