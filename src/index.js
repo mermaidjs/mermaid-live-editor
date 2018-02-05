30d73e22b483ef5e648482f32c3ecf778ee4e454
Tyler Long <tyler4long@gmail.com>
2018-02-05 10:11:37 +0800

    Remove useless code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import mermaid from 'mermaid'
 import 'antd/dist/antd.css'
 
 import App from './components/App'
 import './index.css'
-
-mermaid.initialize({ theme: 'forest' })
 
 ReactDOM.render(<App />, document.getElementById('root'))
