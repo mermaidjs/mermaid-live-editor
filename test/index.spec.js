6ec7c2a16c5a4d61bf92ba0ec43250554eca0e42
Tyler Long <tyler4long@gmail.com>
2018-03-13 13:38:35 +0800

    "mermaid": "^8.0.0-alpha.5"

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,7 +3,6 @@
 import React from 'react'
 import { mount, configure } from 'enzyme'
 import { Input } from 'antd'
-import { Base64 } from 'js-base64'
 import Adapter from 'enzyme-adapter-react-16'
 import 'mermaid'
 
@@ -23,11 +22,8 @@ const verifyTextArea = (code) => {
 }
 
 const verifyLinks = (code) => {
-  const links = wrapper.find('.links').find('a')
-  expect(links.length).toEqual(2)
-  expect(links.at(0).props().children).toEqual('Link to View')
-  expect(links.at(0).props().href).toEqual('#/view/' + Base64.encodeURI(code))
-  expect(links.at(1).props().children).toEqual('Download SVG')
+  const links = wrapper.find('.marketing-links').find('li')
+  expect(links.length).toEqual(4)
 }
 
 test('/', () => {
