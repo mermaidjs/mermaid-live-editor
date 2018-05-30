853974497596b83175aa21eefcf89aea3db4c788
Tyler Long <tyler4long@gmail.com>
2018-05-30 13:45:12 +0800

    Support fontawesome

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'antd/dist/antd.css'
+import '@fortawesome/fontawesome-free-webfonts/css/fa-brands.css'
+import '@fortawesome/fontawesome-free-webfonts/css/fa-regular.css'
+import '@fortawesome/fontawesome-free-webfonts/css/fa-solid.css'
+import '@fortawesome/fontawesome-free-webfonts/css/fontawesome.css'
 
 import App from './components/App'
 import './index.css'
--- src/utils.js
+++ src/utils.js
@@ -23,7 +23,7 @@ A[Christmas] -->|Get money| B(Go shopping)
 B --> C{Let me think}
 C -->|One| D[Laptop]
 C -->|Two| E[iPhone]
-C -->|Three| F[Car]
+C -->|Three| F[fa:fa-car Car]
 `
 export const defaultState = {
   code: defaultCode,
