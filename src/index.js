da594c4b721a0a58f52319fb4757eb2109e843c8
William Cooke <pipeston@gmail.com>
2019-04-24 12:28:38 +0100

    Update fontawesome to latest version

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'antd/dist/antd.css'
-import '@fortawesome/fontawesome-free-webfonts/css/fa-brands.css'
-import '@fortawesome/fontawesome-free-webfonts/css/fa-regular.css'
-import '@fortawesome/fontawesome-free-webfonts/css/fa-solid.css'
-import '@fortawesome/fontawesome-free-webfonts/css/fontawesome.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 import App from './components/App'
 import './index.css'
