2c885a6996ec347c2058e94b06b048a5b280381e
Ashish Jain <ashish.jain@inkclub.com>
2019-11-07 21:34:35 +0100

    #68 Class diagram parser breaks depending on length of content between brackets

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -57,6 +57,13 @@ class Preview extends React.Component {
     } = this.props
     try {
       mermaid.parse(code)
+      // Replacing special characters '<' and '>' with encoded '&lt;' and '&gt;'
+      let _code= code;
+      _code = _code.replace(/</g,'&lt;');
+      _code = _code.replace(/>/g,'&gt;');
+    
+     // Overriding the innerHTML with the updated code string 
+      this.container.innerHTML = _code;
       mermaid.init(undefined, this.container)
     } catch (e) {
       // {str, hash}
